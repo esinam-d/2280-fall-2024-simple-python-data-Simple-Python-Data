7aea4943e66b1b7a36774f13ce9e4499a24a5cff
hoanbklucky <ngothanhhoan@gmail.com>
2024-08-15 21:25:58 -0400

    parametrize hw1_0_test.py

---
+++ conftest.py
@@ -0,0 +1,4 @@
+def pytest_addoption(parser):
+    parser.addoption("--a", action="store", default=1, help="Value of a.")
+    parser.addoption("--b", action="store", default=2, help="Value of a.")
+    parser.addoption("--sum", action="store", default=3, help="Value of a.")
--- hw1_0_test.py
+++ hw1_0_test.py
@@ -1,10 +1,24 @@
 import hw1_0
+import pytest
 
 
-def test_sum():
+@pytest.fixture
+def a_b_sum(request):
+    a = float(request.config.getoption('--a'))
+    b = float(request.config.getoption('--b'))
+    sum = float(request.config.getoption('--sum'))
+    print(a, b, sum)
+    return a, b, sum
+
+
+
+
+def test_sum(a_b_sum):
+    a, b, sum = a_b_sum
     assert hw1_0.sum(1,2) == 3, 'Failed when a = 1 and b = 2'
     assert hw1_0.sum(2,-3) == -1, 'Failed when a = 2 and b = -3'
     assert hw1_0.sum(3,-4.5) == -1.5, 'Failed when a = 3 and b = -4.5'
+    assert hw1_0.sum(a,b) == sum, 'Failed teacher test'
     print("All tests passed")
 
 def main():
