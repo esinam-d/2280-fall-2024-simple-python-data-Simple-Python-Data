0d8cc1df12a7a1c5ad4ad45c3e59258721afa985
esinam-d <esinam.prisdiame@gmail.com>
2024-09-02 01:32:10 +0000

    Update hw1_0.py

--- hw1_0.py
+++ hw1_0.py
@@ -2,5 +2,8 @@
 
 if __name__ == "__main__":
     # Get the two numbers from the user
+    num1= float(input("Input the first number:"))
+    num2= float(input("Input the second number:"))
 
     # Calculate and print the sum of the two numbers
+    print("The sum of the two numbers =", num1+num2)
